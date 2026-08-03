aa88175c6acfb4b1340ceb9c94c6ecddad508f4d
Qihao <Kaiho.cd@gmail.com>
2026-08-03 16:30:07 +0800

    feat(global): modified plugin style

--- configs/zsh/plugins.zsh
+++ configs/zsh/plugins.zsh
@@ -1,6 +1,10 @@
 # Plugins
 source "$HOMEBREW_PREFIX/opt/zinit/zinit.zsh"
 
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=magenta,bold'
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=red,bold'
 zinit light zsh-users/zsh-history-substring-search
+
 zinit light zsh-users/zsh-autosuggestions
+
 zinit light zsh-users/zsh-syntax-highlighting
